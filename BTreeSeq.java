3ad792e1c6ff72455fac18eae48de7f7677dd42b
David C. Larsen <dcl9934@cs.rit.edu>
2011-05-07 16:26:37 -0400

    Moved the Node implementation to a separate file.
    It makes much more sense there than defined as an inner class
    of BTreeSeq.

--- BTreeSeq.java
+++ BTreeSeq.java
@@ -15,48 +15,6 @@ public class BTreeSeq<K,V> implements BTree<K,V>
 	private int numKeysPerNode = 7;
 	private int size = 0;
 
-	class Node<K,V>
-	{
-		private int numKeys;
-		private K[] keys;
-		private Node<K,V>[] children;
-		private Node<K,V> parent = null;
-
-		@SuppressWarnings({"unchecked"})
-		public Node( K key,
-		             V value,
-		             Node<K,V> parent )
-		{
-			// Like: keys = new K[numKeysPerNode], but working around Java's
-			// type-erasure approach to generics.
-			// This cast will always work because Array dynamically creates the
-			// generic array of the correct type. Still, we have to do an
-			// "unchecked" cast, and we don't want to be warned about it,
-			// because we've already guaranteed the type safety.
-			keys = (K[]) Array.newInstance( key.getClass(), numKeysPerNode );
-
-			children = new Node[numKeysPerNode-1];
-
-			this.parent = parent;
-		}
-
-		/**
-		 * Find the lowest number in the range of Keys in this Node.
-		 */
-		public K lowerBound()
-		{
-			return keys[0];
-		}
-
-		/**
-		 * Find the highest number in the range of Keys in this Node.
-		 */
-		public int upperUpper()
-		{
-			return keys[numKeys-1];
-		}
-	}
-
 	private Node<K,V> root = null;
 	
 	/** {@inheritDoc} */
---
+++ Node.java
@@ -0,0 +1,57 @@
+/*
+ * Sequential B*-Tree implementation for the 
+ * Concurrent Search Tree Project for
+ * Parallel Computing I
+ *
+ * Author: David C. Larsen <dcl9934@cs.rit.edu>
+ * Date: April. 12, 2011
+ */
+
+import java.lang.reflect.Array;
+
+/** 
+ * A B-Tree node.
+ */
+class Node<K,V>
+{
+	private final int numKeysPerNode = 7;
+
+	private int numKeys;
+	private K[] keys;
+	private Node<K,V>[] children;
+	private Node<K,V> parent = null;
+
+	@SuppressWarnings({"unchecked"})
+	public Node( K key,
+				 V value,
+				 Node<K,V> parent )
+	{
+		// Like: keys = new K[numKeysPerNode], but working around Java's
+		// type-erasure approach to generics.
+		// This cast will always work because Array dynamically creates the
+		// generic array of the correct type. Still, we have to do an
+		// "unchecked" cast, and we don't want to be warned about it,
+		// because we've already guaranteed the type safety.
+		keys = (K[]) Array.newInstance( key.getClass(), numKeysPerNode );
+
+		children = new Node[numKeysPerNode-1];
+
+		this.parent = parent;
+	}
+
+	/**
+	 * Find the lowest number in the range of Keys in this Node.
+	 */
+	public K lowerBound()
+	{
+		return keys[0];
+	}
+
+	/**
+	 * Find the highest number in the range of Keys in this Node.
+	 */
+	public K upperUpper()
+	{
+		return keys[numKeys-1];
+	}
+}
