18138ce199ecb17d9a341bd58689af408cf96ee9
BENJAMIN DAVID MAYES <bdm8233@dargo.(none)>
2011-05-07 16:14:04 -0400

    Finished commenting the interface file.

--- BTree.java
+++ BTree.java
@@ -4,7 +4,8 @@
  * Parallel Computing I
  *
  * Author: David C. Larsen <dcl9934@cs.rit.edu>
- * Date: April 12, 2011
+ * Author: Benjamin David Mayes <bdm8233@rit.edu>
+ * Date: May 7, 2011
  */
 
 public interface BTree<K,V>
@@ -32,17 +33,40 @@ public interface BTree<K,V>
 
 	/**
 	 * Grab the value associated with the <b>key</b>.
+     *
+     * @param key The key to search for.
+     * @return The value associated with <b>key</b> if one exists or null
 	 */
 	public V get( K key );
 
 	/**
 	 * Determine if the tree is empty.
+     *
+     * @return True if the tree is empty, false otherwise.
 	 */
 	public boolean isEmpty();
 
-	public V put( K key, V value );
+    /**
+     * Sets <b>value</b> as the value associated with <b>key</b>.
+     *
+     * @param key The key to set the value for.
+     * @param value The value to set.
+     * @return The old value associted with <b>key</b>
+     */
+	public V put( K key, V value ); //TODO: Do we want to throw an exception?
 
+    /**
+     * Removes the value associated with <b>key</b>.
+     *
+     * @param key The key to remove the value for.
+     * @return The value formerly associated with <b>key</b> or null.
+     */
 	public V remove( K key );
 
+    /**
+     * Obtains the count of the elements in the tree.
+     *
+     * @return The number of (key,value) pairs in the tree.
+     */
 	public int size();
 }
