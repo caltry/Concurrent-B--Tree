ddee1f4dff805953f1921c2a8492fc0de7f950b0
David C. Larsen <dcl9934@cs.rit.edu>
2011-05-07 23:00:47 -0400

    Added a LeafNode.addValue() function.

--- LeafNode.java
+++ LeafNode.java
@@ -8,4 +8,23 @@ class LeafNode<K extends Comparable, V> extends Node<K,V> {
     public Union.Right<Node<K,V>,V> getChild( K key ) {
        return null;
     }
+	
+	/**
+	 * Adds a key:value pair to the current LeafNode.
+	 */
+	public boolean addValue( K key, V value )
+	{
+		if( numKeys == numKeysPerNode )
+		{	
+			// This node is full. Move on to the next one.
+			return false;
+		}
+		else
+		{
+			keys[numKeys] = key;
+			children[numKeys] = value;
+			numKeys++;
+			return true;
+		}
+	}
 }
