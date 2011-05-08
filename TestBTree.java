7145233d5443a13c03c9839e09de493141dedb89
David C. Larsen <dcl9934@cs.rit.edu>
2011-05-08 19:52:16 -0400

    Added a very basic test of BTree.put().
    It can at least tell us if we crash when we're resizing.

--- TestBTree.java
+++ TestBTree.java
@@ -2,7 +2,7 @@ public class TestBTree
 {
 	public static void main(String[] args)
 	{
-		BTree seqBTree = new BTreeSeq();
+		BTree<Integer, Integer> seqBTree = new BTreeSeq<Integer, Integer>();
         Integer k = new Integer(1);
         Integer v = new Integer(11);
 
@@ -14,5 +14,21 @@ public class TestBTree
 		root.addValue( new Integer(6), new Integer(11) );
 		root.addValue( new Integer(7), new Integer(11) );
 		root.addValue( new Integer(8), new Integer(11) );
+
+        testInsertion( seqBTree );
 	}
+    
+    /**
+     * Test to make sure that the {@link BTree} doesn't crash when we try
+     * to insert things into it.
+     *
+     * @param tree The BTree that we'll be (barely) testing.
+     */
+    public static void testInsertion(BTree<Integer,Integer> tree)
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            tree.put( i, i*10 );
+        }
+    }
 }
